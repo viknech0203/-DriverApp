6cea1db3207d8d66e74894da311c8b7d051d5a13
Lemeshkov <106582873+Lemeshkov@users.noreply.github.com>
2025-05-19 11:59:16 +0300

    подсветка кнопок меню

--- screens/AppHeader.tsx
+++ screens/AppHeader.tsx
@@ -1,126 +1,4 @@
-// import React from 'react';
-// import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-// import { Ionicons } from '@expo/vector-icons';
-// import { Driver } from './DriverInfo';
-// import { useTabs } from './TabsContext';
 
-// interface AppHeaderProps {
-//   screenName: string;
-
-//   driverName?: string;
-//   driver?: Driver;
-//   onMenuToggle: () => void;
-//   menuOpen: boolean;
-// }
-
-// export default function AppHeader({
-//   screenName,
-
-//   driverName = 'Неизвестный водитель',
-//   driver,
-//   onMenuToggle,
-//   menuOpen,
-// }: AppHeaderProps) {
-//   const { setIndex } = useTabs();
-
-//   return (
-//   <View style={styles.wrapper}>
-//   <View style={styles.header}>
-//     <TouchableOpacity
-//       onPress={() => {
-//         console.log('Menu button pressed');
-//         onMenuToggle();
-//       }}
-//       hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
-//     >
-//       <Ionicons name="menu" size={28} color="black" />
-//     </TouchableOpacity>
-
-//     <Text style={styles.screenName}>{screenName}</Text>
-//   </View>
-
-//   <View style={styles.bottomRow}>
-//     <Text style={styles.driver}>{driverName}</Text>
-//   </View>
-
-//   {menuOpen && (
-//     <View style={styles.menu}>
-//       <TouchableOpacity onPress={() => { onMenuToggle(); setIndex(0); }}>
-//         <Text style={styles.menuItem}>Информация о рейсе</Text>
-//       </TouchableOpacity>
-//       <TouchableOpacity onPress={() => { onMenuToggle(); setIndex(1); }}>
-//         <Text style={styles.menuItem}>Чат</Text>
-//       </TouchableOpacity>
-//       <TouchableOpacity onPress={() => { onMenuToggle(); setIndex(2); }}>
-//         <Text style={styles.menuItem}>Статус рейса</Text>
-//       </TouchableOpacity>
-//       <TouchableOpacity onPress={() => {
-//         onMenuToggle();
-//         driver ? setIndex(3) : alert('Информация о водителе недоступна');
-//       }}>
-//         <Text style={styles.menuItem}>Информация о водителе</Text>
-//       </TouchableOpacity>
-//     </View>
-//   )}
-// </View>
-
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   wrapper: {
-//   backgroundColor: '#B7C8E1',
-//   borderBottomWidth: 1,
-//   borderBottomColor: '#ddd',
-//   paddingTop: 30, 
-//   paddingBottom: 10,
-//   position: 'relative',
-//   overflow: 'visible',
-//   minHeight: 100,  // высота шапки
-// },
-//   header: {
-//     padding: 12,
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     alignItems: 'center',
-//     //  height: 110, 
-//   },
-//   screenName: {
-//     fontSize: 18,
-//     fontWeight: 'bold',
-//     flex: 1,
-//     textAlign: 'center',
-//   },
-
-//   bottomRow: {
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     paddingHorizontal: 12,
-//   },
- 
-//   driver: {
-//     color: '#333',
-//   },
-//   menu: {
-//     backgroundColor: '#E3ECF6',
-//     padding: 12,
-//     position: 'absolute',
-//     top: 70, // немного ниже шапки (header + bottomRow)
-//     left: 0,
-//     right: 0,
-//     zIndex: 10,
-//     shadowColor: '#000',
-//     shadowOpacity: 0.2,
-//     shadowOffset: { width: 0, height: 2 },
-//     shadowRadius: 4,
-//     elevation: 5, // для андроида
-//   },
-//   menuItem: {
-//     paddingVertical: 8,
-//     fontSize: 16,
-//     color: '#333',
-//   },
-// });
 
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Dimensions, Pressable } from 'react-native';
@@ -145,7 +23,7 @@ export default function AppHeader({
   onMenuToggle,
   menuOpen,
 }: AppHeaderProps) {
-  const { setIndex } = useTabs();
+  const {  index,setIndex } = useTabs();
 
   return (
     <View style={styles.wrapper}>
@@ -174,19 +52,19 @@ export default function AppHeader({
           {/* Меню слева */}
           <View style={styles.menu}>
             <TouchableOpacity onPress={() => { onMenuToggle(); setIndex(0); }}>
-              <Text style={styles.menuItem}>Информация о рейсе</Text>
+              <Text style={[styles.menuItem, index === 0 && styles.activeItem]}>Информация о рейсе</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => { onMenuToggle(); setIndex(1); }}>
-              <Text style={styles.menuItem}>Чат</Text>
+              <Text style={[styles.menuItem, index === 1 && styles.activeItem]}>Чат</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => { onMenuToggle(); setIndex(2); }}>
-              <Text style={styles.menuItem}>Статус рейса</Text>
+              <Text style={[styles.menuItem, index === 2 && styles.activeItem]}>Статус рейса</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => {
               onMenuToggle();
               driver ? setIndex(3) : alert('Информация о водителе недоступна');
             }}>
-              <Text style={styles.menuItem}>Информация о водителе</Text>
+              <Text style={[styles.menuItem, index === 3 && styles.activeItem]}>Информация о водителе</Text>
             </TouchableOpacity>
           </View>
         </>
@@ -255,4 +133,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#333',
   },
+  activeItem: {
+  backgroundColor: '#cddff1',
+  borderRadius: 8,
+  fontWeight: 'bold',
+  paddingHorizontal: 8,
+},
 });
